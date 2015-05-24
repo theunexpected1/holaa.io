3f9c8e2f3ef5f9fc2cd317ff7f509b3bf7f034d0
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-05-24 18:49:53 +0400

    new: enter screen ui

--- public/js/src/index.js
+++ public/js/src/index.js
@@ -1,6 +1,19 @@
 // App
 
-angular.module('app', [])
-	.controller('appController', function(){
-
-	});
+var app = angular.module('app', ['ngMaterial'])
+	.config( function($mdThemingProvider){
+	// Configure a dark theme with primary foreground yellow
+		$mdThemingProvider.theme('default')
+			.primaryPalette('pink')
+			.accentPalette('blue')
+	})
+	.controller('appController', [
+		'$scope', 
+		function($scope){
+			$scope.channel = '#general';
+			$scope.user = {};
+			$scope.$watch('channel', function(value){
+				$scope.channel = value.indexOf('#') == 0 ? value : '#' + value;
+			});
+		}
+	]);
