665aa1fd199d77837792e93488e1000e660d957a
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-05-25 23:45:51 +0400

    update: socket functionality for login, disconnect and message supported for server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express'),
 	app = express(),
 	http = require('http').Server(app),
 	io = require('socket.io')(http),
+	_ = require('lodash'),
 	port = 8080,
 	users = [],
 	server;
@@ -18,10 +19,26 @@ var express = require('express'),
  	io.on('connection', function(socket){
  		console.log('socket:connected!');
 
- 		socket.on('login', function(user){
- 			console.log('socket:login');
- 			users.push(user);
- 			io.sockets.emit('login', users);
- 		});
- 		
+		socket.on('login', function(user){
+			console.log('socket:login');
+			users.push(user);
+			socket.userDetails = user;
+			io.sockets.emit('login', {
+				message: 'New user has joined',
+				user: user,
+				users: users
+			});
+			console.log(socket);
+		});
+
+		socket.on('message', function(json){
+			console.log('socket:message');
+			io.sockets.emit('message', json);
+		});
+
+		socket.on('disconnect', function(){
+			console.log('socket:disconnect');
+			io.sockets.emit('userLeft', socket.userDetails);
+			_.remove(users, socket.userDetails);
+		})
  	});
