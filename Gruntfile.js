10db3977c76a0393e189e48a9636903dcb6b70a4
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-05-25 14:11:31 +0400

    update: updates to setup and build processes

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,17 +11,62 @@ module.exports = function(grunt){
 					watch: true
 				},
 				files: {
-					'./public/js/build/bundle.js': ['public/js/src/**/*.js']
+					'./public/js/build/main.js': ['public/js/src/**/*.js']
 				}
 			},
 			dist: {
 				files: {
-					'./public/js/build/bundle.js': ['public/js/src/**/*.js']
+					'./public/js/build/main.js': ['public/js/src/**/*.js']
 				}
 			}
+		},
+
+		sass: {
+			dev: {
+				files: {
+					'./public/css/build/main.css': ['./public/css/src/**/*.scss', './public/css/src/**/*.sass']
+				}
+			},
+			dist: {
+				files: '<%=sass.dev.files%>'
+			}
+		},
+
+		watch: {
+			dev: {
+				files: './public/css/build/main.css',
+				tasks: ['sass:dev']
+			},
+			dist: {
+				files: '<%=watch.dev.files%>',
+				tasks: ['sass:dev']
+			},
+		},
+
+		concurrent: {
+			dev: {
+				options: {
+					logConcurrentOutput: true
+				},
+				tasks: ['browserify:dev', 'sass:dev', 'watch:dev']
+			},
+			dist: {
+				options: {
+					logConcurrentOutput: true
+				},
+				tasks: ['browserify:dist', 'sass:dist']
+			}
 		}
+
 	});
 
 	grunt.loadNpmTasks('grunt-browserify');
-	grunt.registerTask('default', ['browserify:' + mode]);
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-concurrent');
+
+	grunt.registerTask('default', ['sass:' + mode, 'browserify:' + mode]);
+	grunt.registerTask('dev', ['concurrent:dev']);
+
+	grunt.registerTask('dist', ['concurrent:dist']);
 };
