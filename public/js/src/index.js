72514d012faf56b791d34e727bede10e7dca8b15
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-05-25 15:28:00 +0400

    update: header layout update to show channel name, and disallow empty channel names

--- public/js/src/index.js
+++ public/js/src/index.js
@@ -4,21 +4,29 @@ var app = angular.module('app', ['ngMaterial', 'ngAnimate'])
 	.config( function($mdThemingProvider){
 		$mdThemingProvider.theme('default')
 			.primaryPalette('pink')
-			.accentPalette('blue')
+			.accentPalette('yellow')
 	})
 	.controller('appController', [
 		'$scope',
 		function($scope){
-			$scope.channel = '#general';
+			var defaultChannelName = '#general';
+			$scope.channel = defaultChannelName;
 			$scope.user = {};
 			$scope.userReadyToChat = false;
 			$scope.$watch('channel', function(value){
-				$scope.channel = value.indexOf('#') == 0 ? value : '#' + value;
+				$scope.channel = $scope.fixChannelName(value, false);
 			});
+
+			$scope.fixChannelName = function(value, checkForEmpty){
+				if(checkForEmpty && (!value || value === '' || value === '#')){
+					value = defaultChannelName;
+				}
+				$scope.channel = value.indexOf('#') == 0 ? value : '#' + value;
+				return value;
+			};
 
 			$scope.enterChat = function(){
 				$scope.userReadyToChat = true;
 			};
 		}
 	]);
-	
