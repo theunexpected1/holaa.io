dd88592ac17c3a3bc56da878b814c47aad2e8fbd
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-05-27 14:48:47 +0400

    new: added a service to randomize colors for user names + filter for basic html filtering to bind html in content

--- public/js/src/app.services.js
+++ public/js/src/app.services.js
@@ -13,4 +13,23 @@ angular.module('app.services', [])
 			}
 		};
 		return obj;
-	});
+	})
+	.service('colors',
+		function(){
+			var colors = {
+				randomizeFromConfig: function(){
+					var colors = ['#00838f', '#00695c', '#A9A739', '#259b24', '#01579b', '#c62626', '#3f51b5', '#673ab7', '#f57f17', '#CFB02C', '#FF6D00'];
+					return colors[_.random(0, colors.length)];
+				}
+			};
+			return colors;
+		}
+	)
+	.filter('basicHtmlFilter', [
+		'$sce',
+		function($sce){
+			return function(text){
+        		return $sce.trustAsHtml(text);
+        	};
+		}
+	]);
