8f19be4efb378d8246be9c0e81ea82541b76638d
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-06-24 21:03:22 +0400

    Update: Moved channel model to it's own module

---
+++ modules/channel/server/channel.controller.js
@@ -0,0 +1,17 @@
+// Channel controller
+'use strict';
+
+module.exports = function(){
+	var channelController = {},
+		communication;
+	/**
+	 * Create a channel if it does not already exist
+	 * @param  {Object} req Request object
+	 * @param  {Object} res Response object
+	 * @return {Object}     Communication response object
+	 */
+	channelController.create = function(req, res){
+	}
+
+	return channelController;
+}
