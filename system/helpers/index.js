57bd410d8a89ed336dd83c8205310cc2c8cc89db
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-06-24 21:24:51 +0400

    Update: helper refactor

--- system/helpers/index.js
+++ system/helpers/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// Production
+// Helper manager
 module.exports = function (System) {
 	// Load Helpers
 	var fs = require('fs'),
 		helperFiles = fs.readdirSync('./system/helpers/'),
 		helpers = {};
-		
+
 	helperFiles.forEach(function(helperFile){
 		if(helperFile.indexOf('.js') > -1 && helperFile.indexOf('index.js') < 0) {
 			var helper = require('./' + helperFile)(System);
