aaf76c1e9f587109262ef5f88eed264199efdb7e
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-06-26 17:08:05 +0400

    Update: Channels controller updated to add methods to create/get channels

--- modules/channel/server/channel.controller.js
+++ modules/channel/server/channel.controller.js
@@ -1,9 +1,12 @@
 // Channel controller
 'use strict';
 
-module.exports = function(){
+module.exports = function(System){
 	var channelController = {},
-		communication;
+		mongoose = require('mongoose'),
+		Channel = require('./channel.model'),
+		communication = System.helpers.communication;
+
 	/**
 	 * Create a channel if it does not already exist
 	 * @param  {Object} req Request object
@@ -11,7 +14,34 @@ module.exports = function(){
 	 * @return {Object}     Communication response object
 	 */
 	channelController.create = function(req, res){
-	}
+		var params = {
+			title: req.body.title
+		}
+		var channel = new Channel(params);
+		channel.save(function(err, data){
+			if(err){
+				communication.fail(res, err);
+			}
+			return communication.ok(res, channel);
+		})
+	};
+
+	/**
+	 * Get channels
+	 * @param  {Object} req Request object
+	 * @param  {Object} res Response object
+	 * @return {Object}     Communication response object
+	 */
+	channelController.get = function(req, res){
+		// ToDo: Query here for retrieving channels based on params
+
+		Channel.find().exec(function(err, channels){
+			if(err){
+				return communication.fail(res, err);
+			}
+			return communication.ok(res, channels);
+		});
+	};
 
 	return channelController;
 }
