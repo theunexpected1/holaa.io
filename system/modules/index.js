a6aaba67048c22d81dec5b956fefe5c2b0972420
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-07-17 16:59:15 +0400

    Update: System.modules now object instead of array

--- system/modules/index.js
+++ system/modules/index.js
@@ -5,18 +5,19 @@ module.exports = function (System) {
 	// Load Modules
 	var fs = require('fs'),
 		appModuleFiles = fs.readdirSync('./modules/'),
-		modules = [],
+		modules = {},
 		ignoreModules = []; // Ignore under-development modules
 
 	appModuleFiles.forEach(function(moduleFile){
 		if(ignoreModules.indexOf(moduleFile) === -1 ){
 			var module = require('../../modules/' + moduleFile + '/server/')(System);
-			modules.push(module);
+			modules[module.key] = module;
 		}
 	});
 
 	// Attach module routes to app
-	modules.forEach(function(module){
+	Object.keys(modules).forEach(function(key) {
+		var module = modules[key];
 		System.log.info('Loaded module: ' + module.routeName);
 		System.app.use('/api/' + module.routeName, module.route);
 	});
