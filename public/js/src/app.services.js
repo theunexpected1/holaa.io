51dc43609c7d1ea22c6f4d0af817e3819c37652c
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-07-17 18:48:38 +0400

    Update: Added auto focus directive

--- public/js/src/app.services.js
+++ public/js/src/app.services.js
@@ -51,4 +51,23 @@ angular.module('app.services', [])
 				});
 			}
 		}
-	]);
+	])
+	
+	/**
+	 * Auto focus directive
+	 */
+	.directive('autoFocus', ['$parse', '$timeout', function($parse, $timeout){
+		return {
+			restrict: 'A',
+			link: function link(scope, element, attrs){
+				var model = $parse(attrs.autoFocus);
+				scope.$watch(model, function(value){
+					if(value === true){
+						$timeout(function(){
+							element[0].focus();
+						});
+					}
+				});
+			}
+		}
+	}]);
